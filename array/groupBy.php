e45ac78aff60894e7d3009fa1f124ee0fe677fed
t.yamada <yamadatsukasa1105@gmail.com>
2021-01-31 19:41:23 +0900

    整形

--- array/groupBy.php
+++ array/groupBy.php
@@ -7,31 +7,30 @@
  * @param mixed $key
  * @return array
  */
-function groupBy(array $array, $key) : array
+function groupBy(array $array, $key, ...$otherKeys) : array
 {
     $grouped = [];
 
-    foreach ($array as $index => $value) {  
+    foreach ($array as $index => $value) { 
         $groupKey = null;
-        if (!is_string($key) && is_callable($key)) {  
-            $groupKey = $key($value);   
-        } else if (is_object($value)) { 
-            $groupKey = $value->{$key};  
-        } else {    
-            $groupKey = $value[$key];   
-        }   
-        $grouped[$groupKey][$index] = $value;   
-    }   
+        if (!is_string($key) && is_callable($key)) { 
+            $groupKey = $key($value);
+        } else if (is_object($value)) {
+            $groupKey = $value->{$key};
+        } else {
+            $groupKey = $value[$key];
+        }
+        $grouped[$groupKey][$index] = $value;
+    }
 
-    if (func_num_args() > 2) {  
-        $args = func_get_args();    
-        foreach ($grouped as $groupKey => $value) { 
+    if (!empty($otherKeys)) {
+        foreach ($grouped as $groupKey => $value) {
             $grouped[$groupKey] = call_user_func_array([$this, 'groupBy'], [
                 $value,
-                ...array_slice($args, 2, func_num_args())
-            ]); 
-        }   
-    }   
+                ...$otherKeys
+            ]);
+        }
+    }
 
-    return $grouped; 
+    return $grouped;
 }
