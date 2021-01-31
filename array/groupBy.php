6c6bcd4a4bd4fd5824e74262d84d5810b7f3685b
t.yamada <yamadatsukasa1105@gmail.com>
2021-01-31 18:25:22 +0900

    整形

--- array/groupBy.php
+++ array/groupBy.php
@@ -1,42 +1,37 @@
 <?php
 
 /**
- * Groups an array by a given key. Any additional keys will be used for grouping
- * the next set of sub-arrays.
+ * Groups the elements of an array by a given key.
  * 
- * @param array $arr     The array to be grouped.
- * @param mixed $key,... A set of keys to group by.
- *
+ * @param array $array
+ * @param mixed $key
  * @return array
  */
 function groupBy(array $array, $key) : array
 {
-    if (!is_string($key) && !is_int($key) && !is_float($key) && !is_callable($key)) {
-        trigger_error('array_group_by(): The key should be a string, an integer, a float, or a function', E_USER_ERROR);
-    }
-
-    $isFunction = !is_string($key) && is_callable($key);
-
     $grouped = [];
-    foreach ($array as $index => $value) {
+
+    foreach ($array as $index => $value) {  
         $groupKey = null;
-        if ($isFunction) {
-            $groupKey = $key($value);
-        } else if (is_object($value)) {
-           $groupKey = $value->{$key};
-        } else {
-            $groupKey = $value[$key];
-        }
-        $grouped[$groupKey][$index] = $value;
-    }
+        if (!is_string($key) && is_callable($key)) {  
+            $groupKey = $key($value);   
+        } else if (is_object($value)) { 
+            $groupKey = $value->{$key};  
+        } else {    
+            $groupKey = $value[$key];   
+        }   
+        $grouped[$groupKey][$index] = $value;   
+    }   
 
-    if (func_num_args() > 2) {
-        $args = func_get_args();
-        foreach ($grouped as $groupKey => $value) {
-            $params = array_merge([$value], array_slice($args, 2, func_num_args()));
-            $grouped[$groupKey] = call_user_func_array([$this, 'groupBy'], $params);
-        }
-    }
+    if (func_num_args() > 2) {  
+        $args = func_get_args();    
+        foreach ($grouped as $groupKey => $value) { 
+            $grouped[$groupKey] = call_user_func_array([$this, 'groupBy'], [
+                $value,
+                ...array_slice($args, 2, func_num_args())
+            ]); 
+        }   
+    }   
 
-    return $grouped;
+    return $grouped; 
 }
